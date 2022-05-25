23afee79cc363bd2782ab66280ec760a26ea5015
thanhtrung5763 <thanhtrung5763@gmail.com>
2022-05-25 08:22:36 +0700

    fixXFrameOptions

---
+++ src/main/java/Filter/XFrameOptionsFilter.java
@@ -0,0 +1,26 @@
+package Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "XFrameOptionsFilter", urlPatterns = {"/header.jsp", "/Admin/headerAdmin.jsp"})
+public class XFrameOptionsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("X-Frame-Options", "DENY");
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
